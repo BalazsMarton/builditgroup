20506f4f8435888a7c70c288ab1069fa20c64759
BalazsMarton <balazsmarton90.joomla@gmail.com>
2017-05-23 10:52:03 +0200

    slideshow fix, animate.css, slideshow init

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,14 @@
 document.addEventListener("turbolinks:load", function() {
 if(('.owl-carousel').length) {
          $('.owl-carousel').owlCarousel({
-             items: 1,
-             nav: true,
+         	items: 1,
+         	nav: true,
+         	animateOut: 'slideOutDown',
+         	animateIn: 'flipInX',
+         	loop:true,
+         	autoplay:true,
+         	autoplayTimeout:5000,
+         	autoplayHoverPause:true
          });
          if(window.matchMedia('(max-width: 767px)').matches) {
              var bg = jQuery(".carousel-item");
