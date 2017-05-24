57fb9ee5f9cfecadb6e2b0ee71ddfcfa17a481b4
BalazsMarton <balazsmarton90.joomla@gmail.com>
2017-05-24 11:38:51 +0200

    responsive slider, base slider caption

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,35 +1,34 @@
 document.addEventListener("turbolinks:load", function() {
 if(('.owl-carousel').length) {
-         $('.owl-carousel').owlCarousel({
-         	items: 1,
-         	nav: true,
-         	animateOut: 'slideOutDown',
-         	animateIn: 'flipInX',
-         	loop:true,
-         	autoplay:true,
-         	autoplayTimeout:5000,
-         	autoplayHoverPause:true
-         });
-         if(window.matchMedia('(max-width: 767px)').matches) {
-             var bg = jQuery(".carousel-item");
-             bg.height(jQuery(window).height()*0.5);
-         };
-
-     }
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+    });
+    if(window.matchMedia('(max-width: 767px)').matches) {
+        var bg = jQuery(".carousel-item");
+        bg.height(jQuery(window).height()-62);
+    };
+}
 $("#full-navbar").headroom({
 	"offset": 205,
-  "tolerance": 5,
-  "classes": {
-    "initial": "animated",
-    "pinned": "slideInDown",
-    "unpinned": "slideOutUp"}
+	"tolerance": 5,
+	"classes": {
+		"initial": "animated",
+		"pinned": "slideInDown",
+		"unpinned": "slideOutUp"
+	}
 });
 $(".navbar-toggle").click(function() {
-         if ($("#myNavbar").hasClass("in")) {
-             return $("body").removeClass("no-scroll");
-         } else {
-             return $("body").addClass("no-scroll");
-         }
-    });
+    if ($("#myNavbar").hasClass("in")) {
+        return $("body").removeClass("no-scroll");
+    } else {
+        return $("body").addClass("no-scroll");
+    }
+});
 
 })
